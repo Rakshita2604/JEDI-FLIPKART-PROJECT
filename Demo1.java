570836ebc69b92f0c78ce131aec45c9c1c831baf
rakshita.r1 <rakshita.r1@FK1089-227382L.local>
2024-11-05 10:24:45 +0530

    My first commit

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Proj1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a sample proj");
+
+	}
+
+}
+
